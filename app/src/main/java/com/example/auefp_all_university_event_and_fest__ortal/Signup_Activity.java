aed1d275228212a1053b8a5337053f3c4a634fee
zkarnob <zkarnob@gmail.com>
2023-02-08 06:13:49 +0600

    completed sign_in,sign_in_main activity both java and sml

--- app/src/main/java/com/example/auefp_all_university_event_and_fest__ortal/Signup_Activity.java
+++ app/src/main/java/com/example/auefp_all_university_event_and_fest__ortal/Signup_Activity.java
@@ -4,11 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Signup_Activity extends AppCompatActivity {
 
+    EditText signupName, signupUsername, signupEmail, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        signupName = findViewById(R.id.signup_name);
+        signupEmail = findViewById(R.id.signup_email);
+        signupUsername = findViewById(R.id.signup_username);
+        signupPassword = findViewById(R.id.signup_password);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+        signupButton = findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+                String name = signupName.getText().toString();
+                String email = signupEmail.getText().toString();
+                String username = signupUsername.getText().toString();
+                String password = signupPassword.getText().toString();
+                helperclass helperclass = new helperclass(name, email, username, password);
+                 reference.child(username).setValue(helperclass);
+
+                Toast.makeText(Signup_Activity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Signup_Activity.this, Login_Activity.class);
+                startActivity(intent);
+            }
+        });
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup_Activity.this, Login_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
